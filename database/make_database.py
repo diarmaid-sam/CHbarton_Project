63a62478ef87a26c5b99ce8276f930e981619460
Diarmaid Sam <diarmaidsam@outlook.com>
2023-04-25 17:10:52 +0100

    make_database.py started. Creates the 2 necessary tables locally (if that hasn't been done so already). If they have been created already, it skips over execution as to prevent error (as attempting to make a pre-existing table flags an error)

--- database/make_database.py
+++ database/make_database.py
@@ -5,6 +5,30 @@ conn = sqlite3.connect("shop_inventory.db")
 
 # create cursor to navigate database
 c = conn.cursor()
+# TODO fix this query, needs a functioning IF statement which enables the verifying of existing tables in the shop inventory database
+c.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name='inventory';""")
+
+table_exists = c.fetchall()
+
+if len(table_exists) == 0:
+    print("here2")
+
+    c.execute("""CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    product_name TEXT NOT NULL,
+    product_section TEXT NOT NULL
+    );""")
+
+    c.execute("""CREATE TABLE inventory (
+    id INTEGER PRIMARY KEY,
+    expiry_date TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    date_added TEXT NOT NULL,
+    product_id INTEGER NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products (product_id)
+    );
+        """)
+
 
 
 # commit changes made to database
