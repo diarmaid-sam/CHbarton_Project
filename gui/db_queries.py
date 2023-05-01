6cae0fb7b9413a0bc8f0bb3ff4cf6a9c1400d01a
Diarmaid Sam <diarmaidsam@outlook.com>
2023-05-01 11:58:31 +0100

    modified get_table_data function. When make_table is true, the column data that is passed now has it's name modified (to make the column header more readable). The columns also now stretch to fill the avaialble space. Also modified the GUI of the unknown_frame

---
+++ gui/db_queries.py
@@ -0,0 +1,57 @@
+import ttkbootstrap.tableview as tbtable
+
+import sqlite3
+
+
+## GLOBAL 1: list of all users 
+conn = sqlite3.connect("shop_inventory.db")
+c = conn.cursor()
+c.execute("""SELECT username FROM users;""")
+users_list = c.fetchall()
+c.close()
+
+## queries database based on provided parameters
+def get_table_data(including_rows, condition, make_table, **kwargs):
+    conn = sqlite3.connect("shop_inventory.db")
+    c = conn.cursor()
+    
+    if condition == None:
+        query_structure = """SELECT {cols} FROM INVENTORY 
+        JOIN products ON inventory.product_id = products.product_id 
+        JOIN users ON products.user_id = users.user_id"""
+        query = query_structure.format(cols=", ".join(including_rows))
+    else:
+        query_structure = """SELECT {cols} FROM INVENTORY 
+        JOIN products ON inventory.product_id = products.product_id 
+        JOIN users ON products.user_id = users.user_id
+        WHERE {cond}"""
+        query = query_structure.format(cols=", ".join(including_rows), cond=condition)
+    
+
+    
+
+    c.execute(query)
+   
+    rowdata = c.fetchall()
+    c.close()
+
+    if make_table:
+        coldata = []
+        for i, colname in enumerate(including_rows):
+            dict_entry = {'text':None, 'stretch': True}
+
+            # changes the original column name into a more user-friendly format
+            colname = colname.replace("_", " ")
+            colname = colname.replace("users.", "")
+            colname = colname.replace("products.", "")
+            colname = colname.replace("invenory.", "")
+            dict_entry['text'] = colname
+            coldata.append(dict_entry)
+
+        table = tbtable.Tableview(master=(kwargs.get('master')), 
+                                          coldata=coldata, 
+                                          rowdata=rowdata,
+                                          searchable=kwargs.get('searchable'),
+                                          stripecolor='gray')
+        return table
+    return rowdata
---
+++ gui/toplevel_windows.py
@@ -0,0 +1,253 @@
+from tkinter import *
+from tkinter import Tk
+from tkinter import ttk
+import ttkbootstrap as tb
+import sqlite3
+
+from button_mappings import calc_layout
+from db_queries import *
+
+# what is necessary:
+# scan item - If the barcode is registered in the database, then the fields are automatically filled. If the barcode isn't registered, then the 'register product' comes up.
+# Quantity - The number of products (make it like a calculator, with big buttons for input)
+# Expiry date 
+
+class AddItems(tb.Toplevel):
+    def __init__(self, master, themename="superhero"):
+        super().__init__(title="Add Items", size=(1000, 800), resizable=(False, True))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)  
+        global placeholder_list
+        global calc_key1
+        # lists are necessary as updating values using int or str data_types causes issues
+        placeholder_list = ['', '']
+        calc_key1 = [0]
+        
+        ## DEFAULT FRAME
+        self.default_frame = tb.Frame(self, bootstyle='danger')
+        self.default_frame.grid(column=0, row=0, sticky='nsew')
+        self.default_frame.columnconfigure(0, weight=1)
+        self.default_frame.rowconfigure(0, weight=1)
+
+        default_frame_prompt = tb.Label(self.default_frame, text="scan an item to get started", bootstyle='light')
+        default_frame_prompt.grid(column=0, row=0, sticky='nsew')
+        default_frame_prompt.config(font=('Arial', 20))
+        default_frame_prompt.configure(anchor='center', justify='center')
+
+        # mimic the scanning of a product (can only be called if default_frame is in focus)
+        self.default_frame.bind('<Control-Return>', self.on_scan)
+
+
+        ## possibly create AddItem_frame when a new item item is being added (and then destroy it afterwards, so that it refreshes)
+        
+        self.AddItem_frame()
+        self.default_frame.tkraise()
+        self.default_frame.focus()
+        self.grid()
+
+    def on_scan(self, event):
+        # mimics the scanning of an item TODO: replace this with an if statement verifying whether scanning has taken place
+        if event.state & 4 and event.keysym == 'Return':
+            self.unknown_barcode()
+            return
+    
+    def unknown_barcode(self):
+        self.unknown_frame = tb.Frame(self)
+        self.unknown_frame.grid(column=0, row=0, sticky='nsew')
+        self.unknown_frame.columnconfigure(0, weight=1)
+        self.unknown_frame.rowconfigure(2, weight=1)
+
+        new_item_btn = tb.Button(self.unknown_frame, text='New Item?', padding=(30, 10))
+        new_item_btn.grid(column=0, row=0, pady=10)
+        search_label = tb.Label(self.unknown_frame, text='or Search existing items')
+        search_label.grid(column=0, row=1, sticky='new', pady=(0, 10))
+        search_label.config(anchor='center', font=('Arial', 14))
+
+        table = get_table_data(['users.user_id', 'product_name'], condition=None, make_table=True, master=self.unknown_frame, searchable=True)
+        table.grid(column=0, row=2, sticky='nsew', padx=10, pady=5)
+        
+
+    def calcAPI(self, *args, key1, key2, placeholder_list):
+        if key2 == "add":
+            # if you are entering the expirey date string, the following rules apply (and the relevant date widget is updated)
+            if key1 == 0:
+                # 7 is max characters for the date, hence it prevents further input once 7 characters is reached
+                if len(placeholder_list[key1]) == 7:
+                    return placeholder_list[key1]
+                # stops the addition of a number if it has reached max 7
+                placeholder_list[key1] += args[0]
+                # if 2 characters is reached, is implies the month is entered, and so a slash is automatically entered
+                if len(placeholder_list[key1]) == 2:
+                    placeholder_list[key1] += '/'
+                self.date.set(placeholder_list[key1])
+                return placeholder_list
+            # else, you modify the quantity label widget
+            else:
+                placeholder_list[key1] += args[0]
+                self.quantity.set(placeholder_list[key1])
+                return placeholder_list
+                
+        elif key2 == "back":            
+            # if the last value of the string is a '/' char, remove an additional char from the str
+            if len(placeholder_list[key1]) > 0:
+                if placeholder_list[key1][-1] == '/':
+                    placeholder_list[key1] = placeholder_list[key1][:-1] 
+
+                # even if none of the above apply, still remove the last digit
+                placeholder_list[key1] = placeholder_list[key1][:-1]
+                # update value of label widget
+                if key1 == 0:
+                    self.date.set(placeholder_list[key1])
+                else:
+                    self.quantity.set(placeholder_list[key1])
+
+                return placeholder_list
+            
+            # if you backspace on an empty 'quantity string' then it will take you back to the 'expirey date string'
+            if key1 == 1:
+                calc_key1[0] = 0
+                # calc_key is updated as is the global variable keeping track of the key1 value
+        # else = when the done button is pressed.
+        else:
+            if key1 == 1: 
+                # sumbit placeholder_list to database
+                # TODO needs to submit month and year separately
+                # clear placeholder_list
+                placeholder_list = ['', '']
+                self.date.set(placeholder_list[0])
+                self.quantity.set(placeholder_list[1])
+
+                return placeholder_list
+            elif len(placeholder_list[key1]) == 7:
+                calc_key1[0] = 1
+                return
+
+        
+    # This frame is the one where (once the item type is confirmed) the item's quantity and expiry date are inputted and put into the database
+    def AddItem_frame(self):
+        self.adding_frame = tb.Frame(self, width=1000, height=800)
+        self.adding_frame.grid(column=0, row=0, sticky='nsew')
+        self.adding_frame.columnconfigure(0, weight=15)
+        self.adding_frame.columnconfigure(1, weight=2)
+        self.adding_frame.rowconfigure(0, weight=1)
+        self.adding_frame.rowconfigure(1, weight=7)
+
+        # the frame where the numbers typed will appear to the user. Large box situated top of the screen  
+        self.input_display = tb.Frame(self.adding_frame, width=((1000*6)/8), height=(800/7), relief='solid')
+        self.input_display.grid(column=0, row=0, sticky='nsew', padx=10, pady=15)
+
+        self.input_display.columnconfigure(0, weight=1)
+        self.input_display.rowconfigure(0, weight=1)
+        self.input_display.rowconfigure(1, weight=1)
+
+        # variable to store the inputs of the digit panel   
+        self.date = StringVar()
+        self.quantity = StringVar()
+    
+        self.date.set("month/year")
+        self.quantity.set("e.g. 12")
+
+        self.date_frame = tb.LabelFrame(self.input_display, text=" Date ",border=1, relief='solid', padding=10)
+        self.date_frame.columnconfigure(0, weight=1)
+        self.date_frame.rowconfigure(0, weight=1)
+        self.date_frame.grid(column=0, row=0, sticky='nsew', pady=5, padx=5)
+
+        self.quantity_frame = tb.LabelFrame(self.input_display, text=" Quantity ",border=1, relief='solid', padding=10)
+        self.quantity_frame.columnconfigure(0, weight=1)
+        self.quantity_frame.rowconfigure(0, weight=1)
+        self.quantity_frame.grid(column=0, row=1, sticky='nsew', pady=5, padx=5)
+
+        self.date_label = tb.Label(self.date_frame, textvariable=self.date)
+        self.date_label.grid(column=0, row=0, sticky='ns')
+        self.quantity_label = tb.Label(self.quantity_frame, textvariable=self.quantity)
+        self.quantity_label.grid(column=0, row=0, sticky='ns')
+
+        self.date_label.config(font=(20))
+        self.quantity_label.config(font=(20))
+
+        
+
+    
+        
+        # create instance of calc_layout class so that we can access methods (to then modify text here)
+        digitPanel = calc_layout(master=self.adding_frame, width=((1000*6)/8), height=((800*6)/7), API=self.calcAPI, key1=calc_key1, placeholder_list=placeholder_list)
+        digitPanel.grid(column=0, row=1, padx=25, pady=18, sticky='nsew')
+
+      
+        # buttons frame contains 2 buttons: 
+        # 1) add more items? (enables for another item to be scanned and then for the process to start again)
+        # 2) finish session (closes the pop-up, when all items to be added are done so)
+        self.buttons = tb.Frame(self.adding_frame, width=((1000*3)/8), height=(800), border=8, relief='solid', padding=(10, 5))
+        self.buttons.grid(column=1, row=1, sticky='ew', padx=(20,10))
+        self.buttons.columnconfigure(0, weight=1)
+        self.buttons.rowconfigure(0, weight=1)
+        self.buttons.rowconfigure(1, weight=1)
+
+        addMore = tb.Button(self.buttons, text="Add More", padding=(20, 15), bootstyle="outline-success").grid(column=0, row=0, sticky='sew')
+        endSession = tb.Button(self.buttons, text="Done", padding=(20, 15), bootstyle="outline-warning", command=lambda: self.destroy()).grid(column=0, row=1, sticky='new', pady=(15, 0))
+
+class AddUser(tb.Toplevel):
+    def __init__(self, master, themename='superhero'):
+        super().__init__(title='Add User', size=(400, 200), resizable=(False, False))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        
+        username_entry_label = tb.Label(self, text="Enter the user's name:")
+        username_entry_label.grid(column=0, row=0, sticky='s')
+        
+        self.username_entry = tb.Entry(self, bootstyle='danger')
+        self.username_entry.grid(column=0, row=1, sticky='sew', padx=10)
+        self.username_entry.focus()
+
+        submit_username = tb.Button(self, text='Submit', bootstyle='primary', command=lambda: self.submit())
+        submit_username.grid(column=0, row=2, sticky='n', pady=(20, 0))
+
+        self.bind('<Return>', lambda event: submit_username.invoke())
+
+
+    def submit(self):
+
+        conn = sqlite3.connect('shop_inventory.db')
+        c = conn.cursor()
+        # insert into users table the new name of the new user.
+        c.execute(" INSERT INTO users (username) VALUES ?", self.username_entry.get())
+        
+        conn.commit()
+        c.close()
+        
+        self.destroy()
+
+class DeleteUser(tb.Toplevel):
+    def __init__(self, master, themename='superhero'):
+        super().__init__(title='delete User', size=(400, 500))
+        self.columnconfigure(0, weight=3)
+        self.columnconfigure(1, weight=1)
+        
+        for i, user in enumerate(users_list):
+            self.rowconfigure(i, weight=1)
+
+            username_label = tb.Label(self, text=user[0], relief='solid', padding=(5, 10), bootstyle='warning')
+            username_label.grid(column=0, row=i, sticky='ew', padx=(15))
+            username_label.config(font=("Arial", 15))
+            username_label.configure(anchor="center")
+
+            delete_button = tb.Button(self, text='delete', padding=(0, 10), bootstyle="danger", command=lambda i=i, username=user: self.delete_user(username))
+            delete_button.grid(column=1, row=i, sticky='ew', padx=10)
+
+    def delete_user(self, username):
+        ## TODO: must transfer all items under this user's name to another user (cannot delete otherwise)
+
+        conn = sqlite3.connect("shop_inventory.db")
+        c = conn.cursor()
+
+        c.execute("DELETE FROM users WHERE username=?", (username))
+
+        conn.commit()
+        c.close()
+
+        self.destroy()
+        
+
+
